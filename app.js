3800e7a1f2374be972a6dbb17a4dec3e72d5ad8d
ngyewkong <ngyewkong1996@gmail.com>
2022-04-19 09:15:53 +0800

    handle active button switching in js

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // get the sections
 const sections = document.querySelectorAll('.section');
+const buttonsContainer = document.querySelectorAll('.controls');
+const buttons = document.querySelectorAll('.control');
+const mainContent = document.querySelectorAll('.main-content');
+
+function switchPages() {
+    // Buttons click active class
+    buttons.forEach(button => {
+        button.addEventListener('click', clickNewActive);
+    })
+}
+
+function clickNewActive() {
+    document.querySelector('.active-btn').classList.remove('active-btn');
+    this.classList.add('active-btn');
+}
+
+switchPages()
