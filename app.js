2e77af0750c6f81b2ee5220ea572275f9690d27d
Ng Yew Kong <ngyewkong1996@gmail.com>
2022-04-23 00:46:19 +0800

    added toggle for light/dark mode

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
 // get the sections
-const sections = document.querySelectorAll('.section');
-const buttonsContainer = document.querySelectorAll('.controls');
-const buttons = document.querySelectorAll('.control');
-const mainContentBody = document.querySelectorAll('.main-content');
+const sections = document.querySelectorAll(".section");
+const buttonsContainer = document.querySelectorAll(".controls");
+const buttons = document.querySelectorAll(".control");
+const mainContentBody = document.querySelectorAll(".main-content");
+const toggle = document.querySelector(".theme-btn");
 
 function switchPages() {
-    // buttons click active class
-    // function remove and add active styling to button and section
-    // which will show the clicked button section
-    buttons.forEach(button => {
-        button.addEventListener('click', clickNewActive);
-    })
+  // buttons click active class
+  // function remove and add active styling to button and section
+  // which will show the clicked button section
+  buttons.forEach((button) => {
+    button.addEventListener("click", clickNewActive);
+  });
 
-    mainContentBody.forEach(section => {
-        section.addEventListener('click', showActivePage);
-    })
+  mainContentBody.forEach((section) => {
+    section.addEventListener("click", showActivePage);
+  });
+
+  // handle toggle light/dark mode
+  toggle.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
 }
 
 function clickNewActive(event) {
-    document.querySelector('.active-btn').classList.remove('active-btn');
-    this.classList.add('active-btn');
-    // remove active class when event button click is registered
-    document.querySelector('.active').classList.remove('active');
- 
+  document.querySelector(".active-btn").classList.remove("active-btn");
+  this.classList.add("active-btn");
+  // remove active class when event button click is registered
+  document.querySelector(".active").classList.remove("active");
 }
 
 function showActivePage(event) {
-    console.log(event.target);
+  console.log(event.target);
 
-    // check if event target dataset.id == section.id
-    // add active to the section.id that match
-    sections.forEach(section => {
-        if (event.target.dataset.id == section.id) {
-            section.classList.add('active');
-        }
-    })
+  // check if event target dataset.id == section.id
+  // add active to the section.id that match
+  sections.forEach((section) => {
+    if (event.target.dataset.id == section.id) {
+      section.classList.add("active");
+    }
+  });
 }
 
-switchPages()
+switchPages();
